6eae7f2d6e0a97265a31eb0884b29efec03d7a2b
João Pedro <joaopedro.nc@outlook.com>
2022-12-11 12:30:18 -0300

    update db

--- src/db/connect.php
+++ src/db/connect.php
@@ -1,3 +1,7 @@
 <?php
-$dsn = "mysql:host=localhost:3306;dbname=crud-weather-php";
-$pdo = new PDO($dsn, "root", "");
+$dbname = "";
+$host = "";
+$user = "";
+$password = "";
+
+$pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
